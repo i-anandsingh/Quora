0e67c36527337f191b3a716cdba9578d9581e3a4
i-anandsingh <singh.anand974@gmail.com>
2024-08-11 01:07:06 +0530

    Added Swagger Doc

--- src/main/java/com/quora/apimodels/request/UserDetailsRequestDTO.java
+++ src/main/java/com/quora/apimodels/request/UserDetailsRequestDTO.java
@@ -1,5 +1,6 @@
 package com.quora.apimodels.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "User Details RequestDTO")
 public class UserDetailsRequestDTO {
+    @Schema(name = "username", example = "iamanand", required = true, description = "User name of the user.")
     private String username;
+    @Schema(name = "emailId", example = "anyemail@xyz.com", required = true, description = "Email Id of the user.")
     private String emailId;
+    @Schema(name = "bio", example = "My name is Anand.", required = false, description = "Bio of the user.")
     private String bio;
 }
--- src/main/java/com/quora/controller/UserManagementController.java
+++ src/main/java/com/quora/controller/UserManagementController.java
@@ -5,6 +5,9 @@ import com.quora.mapper.UserDetailsMapper;
 import com.quora.service.business.UserManagementService;
 import com.quora.service.models.request.UserDetailsInputDTO;
 import com.quora.service.models.response.UserDetailsOutputDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +23,12 @@ public class UserManagementController {
     public UserManagementController(UserManagementService userService){
         this.userService = userService;
     }
+
+    @Operation(summary = "Create a user", description = "Enables the user to create an Id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User successfully created."),
+            @ApiResponse(responseCode = "400", description = "Bad Request")
+    })
     @PostMapping("/users")
     public ResponseEntity<UserDetailsOutputDTO> createUser (
             @RequestBody UserDetailsRequestDTO requestDTO
@@ -29,6 +38,11 @@ public class UserManagementController {
         return new ResponseEntity<>(outputDTO, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Fetch user details", description = "Enables the user to fetch their details.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User successfully fetched."),
+            @ApiResponse(responseCode = "400", description = "Bad Request")
+    })
     @GetMapping("/users/{username}")
     public ResponseEntity<UserDetailsOutputDTO> fetchUser(
             @PathVariable String username
@@ -39,6 +53,11 @@ public class UserManagementController {
         return new ResponseEntity<>(outputDTO, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update a user", description = "Enables the user to update their details.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User successfully updated."),
+            @ApiResponse(responseCode = "400", description = "Bad Request")
+    })
     @PutMapping("/users/{username}")
     public ResponseEntity<UserDetailsOutputDTO> updateUser(
             @RequestBody UserDetailsRequestDTO requestDTO,
